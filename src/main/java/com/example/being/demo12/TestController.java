3ae30d43230b71278d90cf8f7d644c94beef185a
xuzhansheng <xuzhansheng@bluemoon.com.cn>
2018-12-27 17:16:20 +0800

    添加日志文件 并添加测试

--- src/main/java/com/example/being/demo12/TestController.java
+++ src/main/java/com/example/being/demo12/TestController.java
@@ -1,6 +1,8 @@
 package com.example.being.demo12;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +14,13 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping(value = "/getUser")
     public Map<String, Object> getUser(@RequestParam Integer id) {
+        LOGGER.error("错误调试 11111");
+        LOGGER.info("info调试 122222");
+
         Map<String, Object> data = new HashMap<>();
         data.put("id", id);
         data.put("userName", "admin");
